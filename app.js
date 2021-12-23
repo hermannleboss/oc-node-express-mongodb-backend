71bd1795430f194a4da96379aee48f7f1b8d9f58
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-23 10:06:54 +0000

    add stuff datas

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('Your requete is recieve');
-    next();
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
-
 app.use((req, res, next) => {
     res.status(201);
     next();
