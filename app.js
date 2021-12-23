3b7215ceaa9fd64f2a7e6f324bb1685512b72cbb
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-23 09:48:30 +0000

    add of middlewares

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 
-const app= express();
-app.use((req,res)=> {
-    res.json({message: 'Your requete is recieve'});
+const app = express();
+app.use((req, res, next) => {
+    console.log('Your requete is recieve');
+    next();
 });
-module.exports=app;
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({message: 'Your requete is recieve'});
+    next();
+});
+
+app.use((req, res) => {
+    console.log('The respond is sent with success');
+});
+module.exports = app;
