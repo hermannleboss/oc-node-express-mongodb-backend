66d5f47ccd0ec0da14b4e4846d0dd6f399c1a154
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-24 14:17:16 +0000

    Create User Controller, router and use off them

---
+++ routes/user.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl=require('../controllers/user');
+router.post('/', userCtrl.signup);
+router.post('/', userCtrl.login);
+module.exports = router;
