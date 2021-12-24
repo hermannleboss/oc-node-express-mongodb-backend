994d8080d8a38e258c68152238ccdb01730f7410
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-24 13:36:01 +0000

    refactor logic in controller files

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,37 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const Thing = require('../models/Things');
+const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({message: 'Object saved'}))
-        .catch(error => res.status(400).json({error}));
-
-});
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-        .then(() => res.status(201).json({message: 'Object Updated'}))
-        .catch(error => res.status(400).json({error}));
-});
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-        .then(() => res.status(200).json({message: 'Object Deleted'}))
-        .catch(error => res.status(400).json({error}));
-});
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({_id: req.params.id})
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({error}));
-});
-router.get('/', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({error}));
-
-});
+router.post('/', stuffCtrl.createThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id',stuffCtrl.deleteThing );
+router.get('/:id', stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllThings);
 
 module.exports = router;
